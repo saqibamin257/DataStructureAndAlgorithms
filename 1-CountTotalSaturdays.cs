651b7a1c6a36848e5953d8c0195393ffc9cb6a87
Saqib Amin <saqibamin257@gmail.com>
2022-11-25 13:04:07 +0000

    Count Total Saturdays in a month

---
+++ 1-CountTotalSaturdays.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAndAlgorithms
+{
+    internal class TotalSaturdays_Result
+    {
+        public int countSaturday(int year, int month)
+        {
+            int count = 0;
+            DateOnly dateOnly = new DateOnly(year, month, 1);
+            int daysInMonth = DateTime.DaysInMonth(year, month);
+            for (int i = 1; i <= daysInMonth; i++)
+            {
+                DateOnly currentDay = new DateOnly(year, month, i);
+                if (currentDay.DayOfWeek.ToString().ToUpper() == "SATURDAY")
+                    count++;
+            }
+            return count;
+        }
+    }
+    public class TotalSaturdays_Solution
+    {
+        public static void Input()
+        {
+            TotalSaturdays_Result objResult = new TotalSaturdays_Result();
+            int year = 2022;
+            int month = 11;
+            int result = objResult.countSaturday(year, month);
+
+            Console.WriteLine($"Total number of Saturdays in year:{year}, month:{month} are: {result}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DataStructureAndAlgorithms;
+
 Console.WriteLine("Hello, World!");
+TotalSaturdays_Solution.Input(); 
