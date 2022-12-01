19433c6e8709a8ef2d981d2cdc4fee37cc0e3cf8
Saqib Amin <saqibamin257@gmail.com>
2022-12-01 12:05:56 +0000

    6-SingleLinkList complete program crud operations

---
+++ 6-SingleLinkedList_CompleteProgram_Crud.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAndAlgorithms
+{
+    public class DSA_SingleLinkedList
+    {
+        public Node head;
+        public class Node 
+        {
+            public int data;
+            public Node next;            
+            //constructor
+            public Node (int d) 
+            {
+                data= d;
+                next = null;
+            }
+        }
+        //Insert data at front, at start.
+        public void push(int new_data) 
+        {
+            Node new_node = new Node (new_data);
+            new_node.next = head;
+            head = new_node;
+        }
+        
+        /* Inserts a new node after the given prev_node. */
+        public void InsertAfter(Node prev_node,int new_data) 
+        {
+            /* 1. Check if the given Node is null */
+            if(prev_node == null) 
+            {
+                Console.WriteLine("The given previous" +
+                              " node cannot be null");
+                return;
+            }
+            /* 2 & 3: Allocate the Node &
+                Put in the data*/
+            Node new_node = new Node (new_data);
+            prev_node.next = new_node;
+        }
+
+        /* Appends a new node at the end. This method is
+           defined inside LinkedList class shown above */
+        public void append(int new_data) 
+        {
+            /* 1. Allocate the Node &
+               2. Put in the data
+               3. Set next as null */
+            
+            Node new_node = new Node(new_data);
+            
+            /* 4. If the Linked List is empty,
+                 then make the new node as head */
+
+            if( head == null) 
+            {
+                head = new Node (new_data);
+                return;
+            }
+
+            /* 4. This new node is going to be the last node,
+               so make next of it as null */
+            new_node.next = null;
+
+            /* 5. Else traverse till the last node */
+            Node last = head;
+
+            while(last.next != null) 
+            {
+                last = last.next;
+            }
+            last.next = new_node;
+            return;
+        }
+
+        public void printList()
+        {
+            Node tnode = head;
+            while (tnode != null)
+            {
+                Console.Write(tnode.data + " ");
+                tnode = tnode.next;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -31,6 +31,37 @@ using DataStructureAndAlgorithms;
 #endregion
 
 #region (5-c) Single Linked List - Add new node in the start
-new SingleLinkList_AddNodeAtFront().CreateInitialNodes();
+//new SingleLinkList_AddNodeAtFront().CreateInitialNodes();
+#endregion
+
+#region (6) Single Link List - Complete Operations
+//DSA_SingleLinkedList llist = new DSA_SingleLinkedList();
+// Insert 6. So linked list becomes 6->NUllist
+//llist.append(6);
+
+// Insert 7 at the beginning.
+// So linked list becomes 7->6->NUllist
+//llist.push(7);
+
+// Insert 1 at the beginning.
+// So linked list becomes 1->7->6->NUllist
+//llist.push(1);
+
+// Insert 4 at the end. So linked list becomes
+// 1->7->6->4->NUllist
+//llist.append(4);
+
+// Insert 8, after 7. So linked list becomes
+// 1->7->8->6->4->NUllist
+//llist.InsertAfter(llist.head.next, 8);
+
+//Console.Write("Created Linked list is: ");
+//llist.printList();
+
+#endregion
+
+
+#region Sort 2D-MatrixArray
+new SortMatrix_Solution().Input();
 #endregion
 
