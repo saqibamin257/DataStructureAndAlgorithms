34522468d5ead08f5e872f9e45c85815fddfd913
Saqib Amin <saqibamin257@gmail.com>
2022-12-26 12:21:21 +0000

    10(b) BinarySearchtree 1-Insertion using recursion 2- InOrder Traversal(left-current-right)

--- 10(b)- BinarySearchTree-InsertionUsingRecursion.cs
+++ 10(b)- BinarySearchTree-InsertionUsingRecursion.cs
@@ -6,83 +6,167 @@ using System.Threading.Tasks;
 
 namespace DataStructureAndAlgorithms
 {
-    public class Node
+    //public class Node
+    //{
+    //    public int key;
+    //    public Node left, right;
+    //    public Node(int item)
+    //    {
+    //        key = item;
+    //        left = right = null;
+    //    }
+    //}
+    //public class BinarySearchTree
+    //{
+    //    // root Node
+    //    public Node root;
+    //    //constructor
+    //    public BinarySearchTree() 
+    //    {
+    //        root = null;
+    //    }
+    //    public BinarySearchTree(int value) 
+    //    {
+    //        root = new Node(value);
+    //    }
+    //    // This method mainly calls insertRec()
+    //   public void Insert(int key) 
+    //    {
+    //        root = InsertRec(root, key);
+    //    }
+
+    //    // A recursive function to insert a new key in BST
+    //    public Node InsertRec(Node root, int key) 
+    //    {
+    //        // If the tree is empty, return a new node
+    //        if (root == null)
+    //        {
+    //            root = new Node(key);
+    //            return root;
+    //        }
+    //        else if (key < root.key) 
+    //        {
+    //            root.left = InsertRec(root.left, key);
+    //        }
+    //        else if(key > root.key) 
+    //        {
+    //            root.right=InsertRec(root.right, key);
+    //        }
+    //        return root;
+    //    }
+    //    //This method mainly calls InOrderRec()
+    //   public void InOrder() 
+    //    {
+    //        InOrderRec(root);
+    //    }
+    //    // A utility function to do inorder traversal of BST
+
+    //    public void InOrderRec(Node root) 
+    //    {
+    //        if (root != null) 
+    //        {
+    //            InOrderRec(root.left);
+    //            Console.Write(root.key + " ");
+    //            InOrderRec(root.right);
+    //        }
+    //    }
+    //}
+
+    //public class BinarySearchTree_Solution
+    //{
+    //    public void Input() 
+    //    {
+    //        BinarySearchTree tree = new BinarySearchTree();
+    //        /* Let us create following BST
+    //              50
+    //           /     \
+    //          30      70
+    //         /  \    /  \
+    //       20   40  60   80 */
+
+    //        tree.Insert(50);
+    //        tree.Insert(30);
+    //        tree.Insert(20);
+    //        tree.Insert(40);
+    //        tree.Insert(70);
+    //        tree.Insert(60);
+    //        tree.Insert(80);
+            
+    //        // Print inorder traversal of the BST
+    //        tree.InOrder();
+
+    //    }
+
+    //}
+    public class Node 
     {
-        public int key;
+        public int Key;
         public Node left, right;
-        public Node(int item)
+        public Node(int item) 
         {
-            key = item;
+            Key = item;
             left = right = null;
         }
     }
+
     public class BinarySearchTree
     {
-        // root Node
         public Node root;
-        //constructor
         public BinarySearchTree() 
         {
             root = null;
         }
-        public BinarySearchTree(int value) 
+        public BinarySearchTree(int key) 
         {
-            root = new Node(value);
+            root = new Node(key);
         }
-        // This method mainly calls insertRec()
-       public void Insert(int key) 
+        public void Insert(int key) 
         {
-            root = InsertRec(root, key);
+            root = InsertNode(root, key);
         }
-
-        // A recursive function to insert a new key in BST
-        public Node InsertRec(Node root, int key) 
+        public Node InsertNode(Node root,int key) 
         {
-            // If the tree is empty, return a new node
-            if (root == null)
+            if (root == null) 
             {
                 root = new Node(key);
                 return root;
             }
-            else if (key < root.key) 
+           else if(key < root.Key) 
             {
-                root.left = InsertRec(root.left, key);
+                root.left = InsertNode(root.left, key);               
             }
-            else if(key > root.key) 
+            else if (key > root.Key)
             {
-                root.right=InsertRec(root.right, key);
+                root.right = InsertNode(root.right, key);
             }
-            return root;
+            return root;            
         }
-        //This method mainly calls InOrderRec()
-       public void InOrder() 
+        public void PrintInOrder() 
         {
-            InOrderRec(root);
+            InOrder(root);
         }
-        // A utility function to do inorder traversal of BST
-
-        public void InOrderRec(Node root) 
+        public void InOrder(Node root) 
         {
             if (root != null) 
             {
-                InOrderRec(root.left);
-                Console.Write(root.key + " ");
-                InOrderRec(root.right);
-            }
+                //left-center-right
+                InOrder(root.left);
+                Console.Write($"{root.Key} ");
+                InOrder(root.right);
+            }            
         }
     }
-
-    public class BinarySearchTree_Solution
+    public class BinarySearchTree_Solution 
     {
         public void Input() 
         {
             BinarySearchTree tree = new BinarySearchTree();
-            /* Let us create following BST
-                  50
-               /     \
-              30      70
-             /  \    /  \
-           20   40  60   80 */
+            //   /* Let us create following BST
+            //              50
+            //           /     \
+            //          30      70
+            //         /  \    /  \
+            //       20   40  60   80 */
 
             tree.Insert(50);
             tree.Insert(30);
@@ -91,12 +175,13 @@ namespace DataStructureAndAlgorithms
             tree.Insert(70);
             tree.Insert(60);
             tree.Insert(80);
-            
+
             // Print inorder traversal of the BST
-            tree.InOrder();
-
+            tree.PrintInOrder();
+            //output 20 30 40 50 60 70 80
+            
         }
-
     }
+
 
 }
